0c2215fe769efb0f7ea0f7602b99854631839ef9
Datta Mundada <dbmundada333@gmail.com>
2021-10-22 11:22:01 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,11 +3,6 @@ import './App.css';
 import ErrorBoundary from './components/ErrorBoundary';
 import MainPage from './components/MainPage';
 
-const tmp = {
-   employeeId: “2003,
-   designation: “Developer”,
-   experience: “3 year”
-};
 
 class App extends React.Component {
   render() {
